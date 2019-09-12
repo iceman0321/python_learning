ebb1a88b93fd46f50292bd15fee3152a67658378
iceman0321 <ylian004@fiu.edu>
2019-09-12 15:12:06 -0700

    iter fact

--- hello.py
+++ hello.py
@@ -3,3 +3,17 @@ a = 10
 b = 35
 c = a+b
 print(c)
+
+
+def fact(n):
+    return fact_iter(n, 1)
+
+
+def fact_iter(number, product):
+    if number == 1:
+        return product
+    print(number-1, "-->", number*product)
+    return fact_iter(number-1, number*product)
+
+
+print(fact(7))
